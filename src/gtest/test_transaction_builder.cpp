BlockchainLabFudan/zcash

Add Sprout support to TransactionBuilder

src/gtest/test_transaction_builder.cpp
@@ -10,7 +10,68 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+extern ZCJoinSplit* params;
+
 static const std::string tSecretRegtest = "cND2ZvtabDbJ1gucx9GWH6XT9kgTAqfb6cotPt5Q5CyxVDhid2EN";
+
+// Fake an empty view
+class TransactionBuilderCoinsViewDB : public CCoinsView {
+public:
+    std::map<uint256, SproutMerkleTree> sproutTrees;
+
+    TransactionBuilderCoinsViewDB() {}
+
+    bool GetSproutAnchorAt(const uint256 &rt, SproutMerkleTree &tree) const {
+        auto it = sproutTrees.find(rt);
+        if (it != sproutTrees.end()) {
+            tree = it->second;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool GetSaplingAnchorAt(const uint256 &rt, SaplingMerkleTree &tree) const {
+        return false;
+    }
+
+    bool GetNullifier(const uint256 &nf, ShieldedType type) const {
+        return false;
+    }
+
+    bool GetCoins(const uint256 &txid, CCoins &coins) const {
+        return false;
+    }
+
+    bool HaveCoins(const uint256 &txid) const {
+        return false;
+    }
+
+    uint256 GetBestBlock() const {
+        uint256 a;
+        return a;
+    }
+
+    uint256 GetBestAnchor(ShieldedType type) const {
+        uint256 a;
+        return a;
+    }
+
+    bool BatchWrite(CCoinsMap &mapCoins,
+                    const uint256 &hashBlock,
+                    const uint256 &hashSproutAnchor,
+                    const uint256 &hashSaplingAnchor,
+                    CAnchorsSproutMap &mapSproutAnchors,
+                    CAnchorsSaplingMap &mapSaplingAnchors,
+                    CNullifiersMap &mapSproutNullifiers,
+                    CNullifiersMap saplingNullifiersMap) {
+        return false;
+    }
+
+    bool GetStats(CCoinsStats &stats) const {
+        return false;
+    }
+};
 
 TEST(TransactionBuilder, Invoke)
 {
@@ -33,6 +94,10 @@ TEST(TransactionBuilder, Invoke)
     auto ivk = fvk.in_viewing_key();
     libzcash::diversifier_t d = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     auto pk = *ivk.address(d);
+
+    auto sproutSk = libzcash::SproutSpendingKey::random();
+    ZCNoteDecryption sproutDecryptor(sproutSk.receiving_key());
+    auto sproutAddr = sproutSk.address();
 
     // Create a shielding transaction from transparent to Sapling
     // 0.0005 t-ZEC in, 0.0004 z-ZEC out, 0.0001 t-ZEC fee
@@ -88,9 +153,106 @@ TEST(TransactionBuilder, Invoke)
     EXPECT_TRUE(ContextualCheckTransaction(tx2, state, 3, 0));
     EXPECT_EQ(state.GetRejectReason(), "");
 
+    // Create a Sapling-to-Sprout transaction (reusing the note from above)
+    // - 0.0004 Sapling-ZEC in      - 0.00025 Sprout-ZEC out
+    //                              - 0.00005 Sapling-ZEC change
+    //                              - 0.0001 t-ZEC fee
+    auto builder3 = TransactionBuilder(consensusParams, 2, nullptr, params);
+    ASSERT_TRUE(builder3.AddSaplingSpend(expsk, note, anchor, witness));
+    builder3.AddSproutOutput(sproutAddr, 25000);
+    auto maybe_tx3 = builder3.Build();
+    ASSERT_EQ(static_cast<bool>(maybe_tx3), true);
+    auto tx3 = maybe_tx3.get();
+
+    EXPECT_EQ(tx3.vin.size(), 0);
+    EXPECT_EQ(tx3.vout.size(), 0);
+    EXPECT_EQ(tx3.vjoinsplit.size(), 1);
+    EXPECT_EQ(tx3.vjoinsplit[0].vpub_old, 25000);
+    EXPECT_EQ(tx3.vjoinsplit[0].vpub_new, 0);
+    EXPECT_EQ(tx3.vShieldedSpend.size(), 1);
+    EXPECT_EQ(tx3.vShieldedOutput.size(), 1);
+    EXPECT_EQ(tx3.valueBalance, 35000);
+
+    EXPECT_TRUE(ContextualCheckTransaction(tx3, state, 3, 0));
+    EXPECT_EQ(state.GetRejectReason(), "");
+
+    // Prepare to spend the Sprout note that was just created
+    SproutMerkleTree sproutTree;
+    libzcash::SproutNote sproutNote;
+    SproutWitness sproutWitness;
+    for (int i = 0; i < ZC_NUM_JS_OUTPUTS; i++) {
+        sproutTree.append(tx3.vjoinsplit[0].commitments[i]);
+
+        auto hSig = tx3.vjoinsplit[0].h_sig(*params, tx3.joinSplitPubKey);
+        try {
+            auto pt = libzcash::SproutNotePlaintext::decrypt(
+                sproutDecryptor,
+                tx3.vjoinsplit[0].ciphertexts[i],
+                tx3.vjoinsplit[0].ephemeralKey,
+                hSig,
+                (unsigned char)i);
+            sproutNote = pt.note(sproutAddr);
+            sproutWitness = sproutTree.witness();
+            break;
+        } catch (const std::exception& e) {
+            // One of the outputs must be ours
+            assert(i + 1 < ZC_NUM_JS_OUTPUTS);
+        }
+    }
+
+    // Fake a view with the Sprout note in it
+    auto rt = sproutTree.root();
+    TransactionBuilderCoinsViewDB fakeDB;
+    fakeDB.sproutTrees.insert(std::pair<uint256, SproutMerkleTree>(rt, sproutTree));
+    CCoinsViewCache view(&fakeDB);
+
+    // Create a Sprout-to-[Sprout-and-Sapling] transaction
+    // - 0.00025 Sprout-ZEC in      - 0.00006 Sprout-ZEC out
+    //                              - 0.00004 Sprout-ZEC out
+    //                              - 0.00005 Sprout-ZEC change
+    //                              - 0.00005 Sapling-ZEC out
+    //                              - 0.00005 t-ZEC fee
+    auto builder4 = TransactionBuilder(consensusParams, 2, nullptr, params, &view);
+    builder4.SetFee(5000);
+    ASSERT_TRUE(builder4.AddSproutInput(sproutSk, sproutNote, sproutWitness));
+    builder4.AddSproutOutput(sproutAddr, 6000);
+    builder4.AddSproutOutput(sproutAddr, 4000);
+    builder4.AddSaplingOutput(fvk.ovk, pk, 5000);
+    auto maybe_tx4 = builder4.Build();
+    ASSERT_EQ(static_cast<bool>(maybe_tx4), true);
+    auto tx4 = maybe_tx4.get();
+
+    EXPECT_EQ(tx4.vin.size(), 0);
+    EXPECT_EQ(tx4.vout.size(), 0);
+    // TODO: This should be doable in two JoinSplits.
+    // There's an inefficiency in the implementation.
+    EXPECT_EQ(tx4.vjoinsplit.size(), 3);
+    EXPECT_EQ(tx4.vjoinsplit[0].vpub_old, 0);
+    EXPECT_EQ(tx4.vjoinsplit[0].vpub_new, 0);
+    EXPECT_EQ(tx4.vjoinsplit[1].vpub_old, 0);
+    EXPECT_EQ(tx4.vjoinsplit[1].vpub_new, 0);
+    EXPECT_EQ(tx4.vjoinsplit[2].vpub_old, 0);
+    EXPECT_EQ(tx4.vjoinsplit[2].vpub_new, 10000);
+    EXPECT_EQ(tx4.vShieldedSpend.size(), 0);
+    EXPECT_EQ(tx4.vShieldedOutput.size(), 1);
+    EXPECT_EQ(tx4.valueBalance, -5000);
+
+    EXPECT_TRUE(ContextualCheckTransaction(tx4, state, 4, 0));
+    EXPECT_EQ(state.GetRejectReason(), "");
+
     // Revert to default
     UpdateNetworkUpgradeParameters(Consensus::UPGRADE_SAPLING, Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT);
     UpdateNetworkUpgradeParameters(Consensus::UPGRADE_OVERWINTER, Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT);
+}
+
+TEST(TransactionBuilder, ThrowsOnSproutOututWithoutParams)
+{
+    auto consensusParams = Params().GetConsensus();
+    auto sk = libzcash::SproutSpendingKey::random();
+    auto addr = sk.address();
+
+    auto builder = TransactionBuilder(consensusParams, 1);
+    ASSERT_THROW(builder.AddSproutOutput(addr, 10), std::runtime_error);
 }
 
 TEST(TransactionBuilder, ThrowsOnTransparentInputWithoutKeyStore)

gigibaeh/bitcoinz

src/gtest/test_transaction_builder.cpp
@@ -12,6 +12,67 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+extern ZCJoinSplit* params;
+
+// Fake an empty view
+class TransactionBuilderCoinsViewDB : public CCoinsView {
+public:
+    std::map<uint256, SproutMerkleTree> sproutTrees;
+
+    TransactionBuilderCoinsViewDB() {}
+
+    bool GetSproutAnchorAt(const uint256 &rt, SproutMerkleTree &tree) const {
+        auto it = sproutTrees.find(rt);
+        if (it != sproutTrees.end()) {
+            tree = it->second;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool GetSaplingAnchorAt(const uint256 &rt, SaplingMerkleTree &tree) const {
+        return false;
+    }
+
+    bool GetNullifier(const uint256 &nf, ShieldedType type) const {
+        return false;
+    }
+
+    bool GetCoins(const uint256 &txid, CCoins &coins) const {
+        return false;
+    }
+
+    bool HaveCoins(const uint256 &txid) const {
+        return false;
+    }
+
+    uint256 GetBestBlock() const {
+        uint256 a;
+        return a;
+    }
+
+    uint256 GetBestAnchor(ShieldedType type) const {
+        uint256 a;
+        return a;
+    }
+
+    bool BatchWrite(CCoinsMap &mapCoins,
+                    const uint256 &hashBlock,
+                    const uint256 &hashSproutAnchor,
+                    const uint256 &hashSaplingAnchor,
+                    CAnchorsSproutMap &mapSproutAnchors,
+                    CAnchorsSaplingMap &mapSaplingAnchors,
+                    CNullifiersMap &mapSproutNullifiers,
+                    CNullifiersMap saplingNullifiersMap) {
+        return false;
+    }
+
+    bool GetStats(CCoinsStats &stats) const {
+        return false;
+    }
+};
+
 TEST(TransactionBuilder, Invoke)
 {
     auto consensusParams = RegtestActivateSapling();
@@ -29,6 +90,10 @@ TEST(TransactionBuilder, Invoke)
     auto ivk = fvk.in_viewing_key();
     libzcash::diversifier_t d = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     auto pk = *ivk.address(d);
+
+    auto sproutSk = libzcash::SproutSpendingKey::random();
+    ZCNoteDecryption sproutDecryptor(sproutSk.receiving_key());
+    auto sproutAddr = sproutSk.address();
 
     // Create a shielding transaction from transparent to Sapling
     // 0.0005 t-ZEC in, 0.0004 z-ZEC out, 0.0001 t-ZEC fee
@@ -81,8 +146,101 @@ TEST(TransactionBuilder, Invoke)
     EXPECT_TRUE(ContextualCheckTransaction(tx2, state, 3, 0));
     EXPECT_EQ(state.GetRejectReason(), "");
 
+    // Create a Sapling-to-Sprout transaction (reusing the note from above)
+    // - 0.0004 Sapling-ZEC in      - 0.00025 Sprout-ZEC out
+    //                              - 0.00005 Sapling-ZEC change
+    //                              - 0.0001 t-ZEC fee
+    auto builder3 = TransactionBuilder(consensusParams, 2, nullptr, params);
+    builder3.AddSaplingSpend(expsk, note, anchor, witness);
+    builder3.AddSproutOutput(sproutAddr, 25000);
+    auto tx3 = builder3.Build().GetTxOrThrow();
+
+    EXPECT_EQ(tx3.vin.size(), 0);
+    EXPECT_EQ(tx3.vout.size(), 0);
+    EXPECT_EQ(tx3.vjoinsplit.size(), 1);
+    EXPECT_EQ(tx3.vjoinsplit[0].vpub_old, 25000);
+    EXPECT_EQ(tx3.vjoinsplit[0].vpub_new, 0);
+    EXPECT_EQ(tx3.vShieldedSpend.size(), 1);
+    EXPECT_EQ(tx3.vShieldedOutput.size(), 1);
+    EXPECT_EQ(tx3.valueBalance, 35000);
+
+    EXPECT_TRUE(ContextualCheckTransaction(tx3, state, 3, 0));
+    EXPECT_EQ(state.GetRejectReason(), "");
+
+    // Prepare to spend the Sprout note that was just created
+    SproutMerkleTree sproutTree;
+    libzcash::SproutNote sproutNote;
+    SproutWitness sproutWitness;
+    for (int i = 0; i < ZC_NUM_JS_OUTPUTS; i++) {
+        sproutTree.append(tx3.vjoinsplit[0].commitments[i]);
+
+        auto hSig = tx3.vjoinsplit[0].h_sig(*params, tx3.joinSplitPubKey);
+        try {
+            auto pt = libzcash::SproutNotePlaintext::decrypt(
+                sproutDecryptor,
+                tx3.vjoinsplit[0].ciphertexts[i],
+                tx3.vjoinsplit[0].ephemeralKey,
+                hSig,
+                (unsigned char)i);
+            sproutNote = pt.note(sproutAddr);
+            sproutWitness = sproutTree.witness();
+            break;
+        } catch (const std::exception& e) {
+            // One of the outputs must be ours
+            assert(i + 1 < ZC_NUM_JS_OUTPUTS);
+        }
+    }
+
+    // Fake a view with the Sprout note in it
+    auto rt = sproutTree.root();
+    TransactionBuilderCoinsViewDB fakeDB;
+    fakeDB.sproutTrees.insert(std::pair<uint256, SproutMerkleTree>(rt, sproutTree));
+    CCoinsViewCache view(&fakeDB);
+
+    // Create a Sprout-to-[Sprout-and-Sapling] transaction
+    // - 0.00025 Sprout-ZEC in      - 0.00006 Sprout-ZEC out
+    //                              - 0.00004 Sprout-ZEC out
+    //                              - 0.00005 Sprout-ZEC change
+    //                              - 0.00005 Sapling-ZEC out
+    //                              - 0.00005 t-ZEC fee
+    auto builder4 = TransactionBuilder(consensusParams, 2, nullptr, params, &view);
+    builder4.SetFee(5000);
+    builder4.AddSproutInput(sproutSk, sproutNote, sproutWitness);
+    builder4.AddSproutOutput(sproutAddr, 6000);
+    builder4.AddSproutOutput(sproutAddr, 4000);
+    builder4.AddSaplingOutput(fvk.ovk, pk, 5000);
+    auto tx4 = builder4.Build().GetTxOrThrow();
+
+    EXPECT_EQ(tx4.vin.size(), 0);
+    EXPECT_EQ(tx4.vout.size(), 0);
+    // TODO: This should be doable in two JoinSplits.
+    // There's an inefficiency in the implementation.
+    EXPECT_EQ(tx4.vjoinsplit.size(), 3);
+    EXPECT_EQ(tx4.vjoinsplit[0].vpub_old, 0);
+    EXPECT_EQ(tx4.vjoinsplit[0].vpub_new, 0);
+    EXPECT_EQ(tx4.vjoinsplit[1].vpub_old, 0);
+    EXPECT_EQ(tx4.vjoinsplit[1].vpub_new, 0);
+    EXPECT_EQ(tx4.vjoinsplit[2].vpub_old, 0);
+    EXPECT_EQ(tx4.vjoinsplit[2].vpub_new, 10000);
+    EXPECT_EQ(tx4.vShieldedSpend.size(), 0);
+    EXPECT_EQ(tx4.vShieldedOutput.size(), 1);
+    EXPECT_EQ(tx4.valueBalance, -5000);
+
+    EXPECT_TRUE(ContextualCheckTransaction(tx4, state, 4, 0));
+    EXPECT_EQ(state.GetRejectReason(), "");
+
     // Revert to default
     RegtestDeactivateSapling();
+}
+
+TEST(TransactionBuilder, ThrowsOnSproutOutputWithoutParams)
+{
+    auto consensusParams = Params().GetConsensus();
+    auto sk = libzcash::SproutSpendingKey::random();
+    auto addr = sk.address();
+
+    auto builder = TransactionBuilder(consensusParams, 1);
+    ASSERT_THROW(builder.AddSproutOutput(addr, 10), std::runtime_error);
 }
 
 TEST(TransactionBuilder, ThrowsOnTransparentInputWithoutKeyStore)